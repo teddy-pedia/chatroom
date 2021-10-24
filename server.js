d08fcecb58c81a425169fd0bd872013f79f089aa
teddypedia <1009626007@qq.com>
2021-10-24 21:13:35 +0800

    initial completion

--- server.js
+++ server.js
@@ -3,27 +3,61 @@ const path = require('path')
 const serve = require('koa-static')
 const http = require('http')
 const socketIO = require('socket.io')
-const hostname = '127.0.0.1'
+const hostname = 'localhost'
 const port = 3000
 // 创建一个Koa实例对象
 const app = new Koa()
 
+const sockets = {}
+const history = []
+
 const server = http.createServer(app.callback())
 const io = socketIO(server)
+// 验证用户
+io.use((socket, next) => {
+    console.log('a client comming')
+    let username = socket.handshake.query.username
+    let password = socket.handshake.query.password
+    if (!username) {
+        console.log('拒绝连接：用户名为空')
+        next(new Error('empty'))
+        return
+    }
+    if (password !== '111') {
+        console.log('拒绝连接：密码错误')
+        next(new Error('error_password'))
+        return
+    }
+    next()
+})
 
 io.on('connection', (socket) => {
-    // 获取登录的用户名和密码
-    const info = socket.handshake.query
-    const username = info.username
-    const password = info.password
-    if (username === 'liningjing' && password === 'hello') {
-        socket.emit('message', 'success')
-    } else {
-        socket.emit('message', 'error')
-    }
-    // 监听标识符为chatMessage发来的消息
-    socket.on('chatMessage', (msg) => {
-        console.log(msg) // hello from client
+    let username = socket.handshake.query.username
+    console.log(`${username}登录`)
+
+    sockets[username] = socket
+
+    // 广播发送在线的用户
+    io.emit('online', Object.keys(sockets))
+
+    socket.on('sendMessage', (content) => {
+        const message = {
+            username,
+            date: new Date(),
+            content,
+        }
+        history.push(message)
+        socket.broadcast.emit('receiveMessage', message)
+    })
+
+    socket.on('getHistory', (fn) => {
+        fn(history)
+    })
+
+    socket.on('disconnect', (reason) => {
+        delete sockets[username]
+        io.sockets.emit('online', Object.keys(sockets))
+        console.log(`${username}断开连接，因为${reason}`)
     })
 })
 
--- web/index.js
+++ web/index.js
@@ -1,35 +1,122 @@
-window.onload = () => {
-    const loginBtn = document.getElementById('login-btn')
+;(function () {
+    function $(query) {
+        return document.querySelector(query)
+    }
+
+    const loginBtn = $('#login-btn')
+    let socket = null
+
     // 给登录按钮添加点击事件后的切换动画
-    loginBtn.addEventListener('click', (e) => {
+    function onLogin() {
         // 获取用户输入的账号和密码
-        let username = document.getElementById('username').value
-        let password = document.getElementById('password').value
+        let username = $('#username').value
+        let password = $('#password').value
 
-        const login = document.getElementById('login-wrap')
-        login.style.animation = 'disappear .5s'
-        login.style.animationFillMode = 'forwards'
+        if (!username) {
+            alert('用户名不能为空')
+            return
+        }
+        if (!password) {
+            alert('密码不能为空')
+            return
+        }
 
-        const chat = document.getElementById('chatroom-wrap')
-        chat.style.animation = 'appear .5s'
-        chat.style.animationFillMode = 'forwards'
-    })
+        socket = io({
+            query: {
+                username: username,
+                password: password,
+            },
+            reconnection: false,
+        })
+
+        // 监听登录返回消息
+        socket.on('connect', () => {
+            const login = $('#login-wrap')
+            login.style.animation = 'disappear .5s'
+            login.style.animationFillMode = 'forwards'
+
+            const chat = $('#chatroom-wrap')
+            chat.style.animation = 'appear .5s'
+            chat.style.animationFillMode = 'forwards'
+
+            // 获取历史消息
+            socket.emit('getHistory', (data) => {
+                let msgBox = $('#msg-box')
+                msgBox.innerHTML = data
+                    .map((val) => {
+                        if (val.username === username) {
+                            // 本人发送
+                            return `
+                                <div class="msg msg-me">
+                                    <img src="./1.png" alt="" class="avator" />
+                                    <div class="text">${val.content}</div>
+                                </div>
+                                `
+                        } else {
+                            // 别人发送
+                            return `
+                                <div class="msg">
+                                    <img src="./1.png" alt="" class="avator" />
+                                    <div class="text">${val.content}</div>
+                                </div>
+                                `
+                        }
+                    })
+                    .join('')
+            })
+
+            // 获取聊天用户列表
+            socket.on('online', (onlines) => {
+                const ul = $('#friend_list')
+                ul.innerHTML = onlines.map((user) => {
+                    return `<li>${user}</li>`
+                })
+            })
+
+            socket.on('receiveMessage', (message) => {
+                let msgBox = $('#msg-box')
+                let newMessage = document.createElement('div')
+                newMessage.innerHTML = `
+                <div class="msg">
+                    <img src="./1.png" alt="" class="avator" />
+                    <div class="text">${message.content}</div>
+                </div>
+                `
+                msgBox.appendChild(newMessage)
+            })
+        })
+
+        // 登录失败监听
+        socket.on('connect_error', (e) => {
+            console.log('connect_error', e.username)
+            if (e && e.message === 'error_password') {
+                alert('密码错误')
+                return
+            }
+        })
+    }
+    loginBtn.addEventListener('click', onLogin)
 
     // 给发送按钮添加点击事件，点击之后发送消息
-    const sendBtn = document.getElementById('send-btn')
-    sendBtn.addEventListener('click', (e) => {
-        let input = document.getElementById('input')
+    function onSendMessage() {
+        let input = $('#input')
         let input_text = input.value
-        let msg = document.createElement('div')
-        msg.innerHTML = `
+        let msgBox = $('#msg-box')
+
+        let div = document.createElement('div')
+        div.innerHTML = `
             <div class="msg msg-me">
                 <img src="./1.png" alt="" class="avator" />
                 <div class="text">${input_text}</div>
             </div>
         `
-        const msgBox = document.getElementById('msg-box')
-        msgBox.appendChild(msg)
+        msgBox.appendChild(div)
+
+        socket.emit('sendMessage', input_text)
+
         // 清空输入框消息
         input.value = ''
-    })
-}
+    }
+    const sendBtn = document.getElementById('send-btn')
+    sendBtn.addEventListener('click', onSendMessage)
+})()
